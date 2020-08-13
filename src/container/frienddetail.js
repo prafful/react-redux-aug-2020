ca31e1e32a0c607698198ea81b784b847b58b8fa
prafful <prraful@gmail.com>
2020-08-13 02:57:42 -0700

    day 01 - redux

--- src/container/frienddetail.js
+++ src/container/frienddetail.js
@@ -1,22 +1,63 @@
 import React from 'react';
+import { connect } from 'react-redux';
 
 class FriendDetail extends React.Component {
+
+    getCountFromPropsOtherFriend =()=>{
+        console.log("Received otherfriend as prop in FriendDetail container....")
+        console.log(this.props.otherfriend);
+        let of = this.props.otherfriend.find(f=>{
+            return f.id == this.props.friend.id
+        })
+        return of.otherfriends
+    }
+
+    getLikesFromPropsOtherFriend =()=>{
+        console.log("Received likes as prop in FriendDetail container....")
+        console.log(this.props.likes);
+        let friendlikes = this.props.likes.find(f=>{
+            return f.id == this.props.friend.id
+        })
+        console.log(friendlikes);
+        return friendlikes.likes
+    }
     
     render() { 
-        return ( 
-            <div>
-                Click friend above to see detail:
-                <br></br>
-                <ol>
-                    <li>Full Name: </li>
-                    <li>Location: </li>
-                    <li>Friend Since: </li>
-                    <li>Likes:</li>
-                    <li>Other Friends: </li>
-                </ol>
-            </div>
-         );
+        if(this.props.friend === null){
+            return (
+                <div>
+                    <h3>Click on any friend above to see detail....</h3>
+                </div>
+            )
+        }else{
+            return ( 
+                <div>
+                    Click friend above to see detail:
+                    <br></br>
+                    <ol>
+                        <li>Full Name: {this.props.friend.name}</li>
+                        <li>Location: {this.props.friend.location} </li>
+                        <li>Friend Since: {this.props.friend.since} </li>
+                        <li>Likes: {this.getLikesFromPropsOtherFriend()} </li>
+                        <li>Other Friends: {this.getCountFromPropsOtherFriend()}</li>
+                    </ol>
+                </div>
+             );
+        }
+
+        
+    }
+}
+
+
+function convertStoreToPropsForFriendDetail(store){
+    console.log("FriendDetail has received store....")
+    console.log(store);
+    return {
+        friend: store.friendclicked,
+        otherfriend: store.otherfriends,
+        likes: store.allfriendslikes
     }
 }
  
-export default FriendDetail;
+export default connect(convertStoreToPropsForFriendDetail)(FriendDetail)
--- src/container/friendlikes.js
+++ src/container/friendlikes.js
@@ -1,18 +1,49 @@
 import React from 'react';
+import { connect } from 'react-redux';
 
 class FriendLikes extends React.Component {
+
+    getLikesFromProps=()=>{
+        console.log("Received likes as prop in FriendLike container....")
+        console.log(this.props.likes);
+        console.log("Received friendclicked as prop in FriendLike container....")
+        console.log(this.props.friend);
+        let friendlikes = this.props.likes.find(f=>{
+            return f.id == this.props.friend.id
+        })
+        console.log(friendlikes);
+        return friendlikes.likes  
+    }
     
     render() { 
+        if(this.props.friend === null){
+            return (
+                <div>
+                    <h3>Click on friend to change likes!!!!</h3>
+                </div>
+            )
+        }
+        else{
         return ( 
             <div>
                 I will change number of likes for given friend:
                 <br></br>
                 <button>+</button>
-                &nbsp;
+                &nbsp; { this.getLikesFromProps()  }
                 <button>-</button>
             </div>
-         );
+          )
+        }
+    }
+}
+
+function convertStoreToPropsForFriendLikesContainer(store){
+    console.log("Received store in FriendLike container....");
+    console.log(store);
+    return {
+        likes: store.allfriendslikes,
+        friend: store.friendclicked
     }
 }
  
-export default FriendLikes;
+export default  connect(convertStoreToPropsForFriendLikesContainer)(FriendLikes);
